da40a4195520f611751efe2e2761f7ab6bdce8d1
Garvit Rai <raigarvit9@gmail.com>
2026-02-03 16:06:33 +0530

    Fixed deprecated mongoose package

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose")
 
 const url = process.env.MONGODB_URL;
 
-mongoose.connect(url, {
-    useNewUrlParser: true,
-}).then(() => {
+mongoose.connect(url).then(() => {
     console.log("Connected to Database");
 }).catch(err => {
     console.error("Error connecting to Database", err);
